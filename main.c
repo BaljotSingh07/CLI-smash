0280bc09e9c25ca6bcc6b8c2f121f345d92163b4
Baljot Singh <baljotvirk07@gmail.com>
2023-02-14 10:35:21 -0600

    makefile

--- main.c
+++ main.c
@@ -2,95 +2,138 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
-char* paths[] = {"/bin/", "/usr/bin/", NULL};
+char *paths[] = {"/bin/", "/usr/bin/", NULL};
 
-int runCommand(char* command){
-    char* argToken;
+int runCommand(char *command[], int commandSize)
+{
+    // see if the redirection symbol is in the command and where is it
+    int indexOfRedirection = -1;
+    for(int i = 0; i < commandSize; i++){
+        if(strcmp(command[i], ">") == 0){
+            indexOfRedirection = i;
+            break;
+        }
+    }
 
-    char* args[10];
+    if(indexOfRedirection != -1){
+        
+        // check for redirection formating
+        if(indexOfRedirection != commandSize - 2){
+            write(STDOUT_FILENO, "Error: unable to parse redirection command\n", 42);
+            return -1;
+        }
+        if(indexOfRedirection == 0){
+            write(STDOUT_FILENO, "Error: no command specified\n", 27);
+            return -1;
+        }
+        // formating is good, redirect output
+
+        int tmpFd = open(command[indexOfRedirection + 1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+
+        dup2(tmpFd, STDOUT_FILENO);
+        dup2(tmpFd, STDERR_FILENO);
+
+        // remove redirection symbols from command
+        command[indexOfRedirection] = NULL;
+        close(tmpFd);
+    }
+
    
-    int i = 0;
-    while( (argToken = strsep(&command," ")) != NULL ){
-        if(argToken[0] == '\0')
-            continue;
-
-        args[i] = argToken;
-        i++;
-    }
-    //make it a null terminated array
-    args[i] = NULL;
-
-    // check for build in command
-    if(strcmp(args[0], "exit") == 0){
-        exit(1);
-    }
-    
-    if(strcmp(args[0], "cd") == 0){
-        chdir(args[1]);
-    }
-
-    if(strcmp(args[0], "path") == 0){
-        //add path
-
-        //remove path
-
-        //clear path
-    }
-   
-   //run execv at user/bin
-    char* path = malloc(128);
+    char *path = malloc(128);
     strcpy(path, "/bin/");
-    strcat(path, args[0]);
-    execv(path, args);
-    return 0;
-    
+    strcat(path, command[0]);
+    execv(path, command);
 }
+
+int getSeqCommands(char* buffer, char* seqCommands[]){
+    int i = 0;
+    char* token = strtok(buffer, ";");
+    while(token != NULL){
+        seqCommands[i] = token;
+        i++;
+        token = strtok(NULL, ";");
+    }
+    seqCommands[i] = NULL;
+    return i;
+}
+
+int getAsyncCommands(char* buffer, char* asyncCommands[]){
+    int i = 0;
+    char* token = strtok(buffer, "&");
+    while(token != NULL){
+        asyncCommands[i] = token;
+        i++;
+        token = strtok(NULL, "&");
+    }
+    asyncCommands[i] = NULL;
+    return i;
+}
+
+int parseCommand(char* unparsedCommand, char* parsedCommand[]){
+    int i = 0;
+    char* token = strtok(unparsedCommand, " ");
+    while(token != NULL){
+        parsedCommand[i] = token;
+        i++;
+        token = strtok(NULL, " ");
+    }
+    parsedCommand[i] = NULL;
+    return i;
+}
+
 
 int main(void)
 {
     
     while(1){
-        char* input = malloc(128); 
+        char* buffer = malloc(128); 
 
         printf("Smash> ");
-        fgets(input, 128, stdin);
+        fgets(buffer, 128, stdin);
 
         //remove newline
-        input[strlen(input) - 1] = '\0';
+        buffer[strlen(buffer)-1] = '\0';
 
-        char* commandToken;
-        char* commands[10];
+        char* seqCommands[10];
+        int seqCommandSize = getSeqCommands(buffer, seqCommands);
 
-        int i = 0;
-        while( (commandToken = strsep(&input,"&")) != NULL ){
-            if(commandToken[0] == '\0')
-                continue;
-        
-            commands[i] = commandToken;
-            i++;
-        }
+        for(int i = 0; i < seqCommandSize; i++){
+            char* asyncCommands[10];
+            int asyncCommandSize = getAsyncCommands(seqCommands[i], asyncCommands);
+            for(int j = 0; j < asyncCommandSize; j++){
+                //check for built in commands
+                
+                //parse command
+                char* parsedCommand[10];
+                int commandSize = parseCommand(asyncCommands[j], parsedCommand);
 
+                if(strcmp(parsedCommand[0], "exit") == 0){
+                    exit(0);
+                }
+                else if(strcmp(parsedCommand[0], "cd") == 0){
+                    chdir(parsedCommand[1]);
+                    continue;
+                }
+                else if(strcmp(parsedCommand[0], "path") == 0){
+                    
+                    continue;
+                }
 
-        //run each commands parralelly
-        for(int j = 0; j < i; j++){
-            int pid = fork();
-            if(pid == 0){
-                runCommand(commands[j]);
-                return 0;
+                int pid = fork();
+                if(pid == 0){
+                    //run command
+                    runCommand(parsedCommand, commandSize);
+                    exit(0);
+                }
+            }
+            for(int j = 0; j < asyncCommandSize; j++){
+                wait(NULL);
             }
         }
-        //wait for all children to finish
-        for(int j = 0; j < i; j++){
-            int status;
-            wait(&status);
-            
-            //check for 256 flag which means quit
-            if(status == 256){
-                exit(0);
-            }
-        }  
-        
     
     }
     return 0;
